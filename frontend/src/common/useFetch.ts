32a41b42b99e755b12d413e8cb465386121ae79a
tanndlin <tanndlin@gmail.com>
2026-09-06 22:12:42 +0000

    Fix buggy tick for refresh

--- frontend/src/common/useFetch.ts
+++ frontend/src/common/useFetch.ts
@@ -1,17 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 
-export default function useFetch<T>(
-    input: URL | RequestInfo | null,
-    init?: RequestInit
-) {
+export default function useFetch<T>(input: URL | RequestInfo | null) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
     const [data, setData] = useState<T | null>(null);
-    const [_, setTick] = useState(0); // Used to trigger refetch
+    const [tick, setTick] = useState(0);
 
     const refetch = useCallback(() => setTick((t) => t + 1), []);
 
     useEffect(() => {
+        let ignore = false;
+
         setLoading(true);
         setError('');
         setData(null);
@@ -21,17 +20,33 @@ export default function useFetch<T>(
             return;
         }
 
-        fetch(input, init)
+        fetch(input)
             .then((res) => {
                 if (!res.ok) {
                     throw new Error(res.statusText);
                 }
                 return res.json() as Promise<T>;
             })
-            .then(setData)
-            .catch((err) => setError(err.message))
-            .finally(() => setLoading(false));
-    }, [input, init]);
+            .then((json) => {
+                if (!ignore) {
+                    setData(json);
+                }
+            })
+            .catch((err) => {
+                if (!ignore) {
+                    setError(err.message);
+                }
+            })
+            .finally(() => {
+                if (!ignore) {
+                    setLoading(false);
+                }
+            });
+
+        return () => {
+            ignore = true;
+        };
+    }, [input, tick]);
 
     return { loading, error, data, refetch };
 }
